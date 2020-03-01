24bbed2a4dd64d7a87f7ec4b19380c84e39b2e6b
chenguo <guochen@shein.com>
2020-03-01 11:16:13 +0800

    add buffer

--- node.js
+++ node.js
@@ -2,6 +2,7 @@ const core = require('./core')
 const http = require('http')
 const https = require('https')
 const zlib = require('zlib')
+const { PassThrough } = require('stream')
 const caseless = require('caseless')
 
 const compression = {
@@ -10,7 +11,32 @@ const compression = {
   deflate: zlib.createInflate
 }
 
-const request = (baseUrl = '', method, statusCode, format, headers) => (_url = '', body, _headers) => {
+const fromatRes = function(res) {
+  const pt = new PassThrough()
+  if (res.headers['content-encoding']) {
+    const arr = res.headers['content-encoding'].split(', ').reverse()
+    for (const encoding of arr) {
+      res = res.pipe(compression[encoding]())
+    }
+  }
+  return res.pipe(pt)
+}
+
+const getBuferr = (res) => new Promise((resolve, reject) => {
+  const arr = []
+  res.on('data', (chunk) => {
+    arr.push(chunk)
+  })
+  res.on('end', () => {
+    console.log('end')
+    resolve(Buffer.concat(arr))
+  })
+  res.on('error', () => {
+    reject()
+  })
+})
+
+const request = (baseUrl = '', method, statusCodes, format, headers) => (_url = '', body, _headers) => {
   _url = baseUrl + _url
   _headers = {...headers, ..._headers}
 
@@ -44,9 +70,18 @@ const request = (baseUrl = '', method, statusCode, format, headers) => (_url = '
 
   return new Promise((resolve, reject) => {
     protocol.request(options, (res) => {
-      console.log(`状态码: ${res.statusCode}`);
-      console.log(`响应头: ${JSON.stringify(res.headers)}`);
-    })
+      console.log(`状态码: ${res.statusCode}`)
+      console.log(`响应头: ${JSON.stringify(res.headers)}`)
+      // @TODO: 把res抛出去
+      if(!statusCodes.has(res.statusCode)) reject(new Error('mismatch status code'))
+
+      // 返回流可能是加密过的，要根据content-encoding判断
+      const response = fromatRes(res)
+      if(!format) return resolve(response)
+
+      const buff = getBuferr(response)
+      //然后要通过各种encoding的方式 对流做变换
+    }) 
   })
 
 }
